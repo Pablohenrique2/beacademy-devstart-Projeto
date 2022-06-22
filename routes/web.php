84e05124fc4dd1fdf7286ede508efb0fc3fde083
Pablo Henrique <pabloenosteteucpx@gnail.com>
2022-06-22 12:02:50 -0300

    adicionando as pastas de css , js e img na pasta public

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $nome = 'jubileu';
+    $idade = 22;
+    return view('welcome', ['nome' => $nome, "ano" => $idade]);
 });
