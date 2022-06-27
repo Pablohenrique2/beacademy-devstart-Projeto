196025786b6d406101c19dadb532e47b68b44260
Pablo Henrique <pabloenosteteucpx@gnail.com>
2022-06-27 15:16:59 -0300

    criando novas models e migrate para categoria,endereço e pedido

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -8,7 +8,7 @@
   <!-- CSS BOOTSTRAP -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
   <!-- CSS DA APLICAÇÃO -->
-  <link rel="stylesheet" href="/css/style.css">
+  <link rel="stylesheet" href="{{asset('/css/style.css')}}">
   <title>@yield('title')</title>
 </head>
 
@@ -123,6 +123,7 @@
 
       </div>
       </div>
+
     </nav>
   </header>
   @yield('content')
